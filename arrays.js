96aa64c692e9748541e53168e5e819264e384059
Mary Alice Orr <m.a.orr17@gmail.com>
2018-02-27 19:50:04 -0600

    second commit

--- arrays.js
+++ arrays.js
@@ -1,1 +1,33 @@
 console.log("hello");
+
+// var test1 = "cat";
+
+// var test2 = "racecar";
+
+// var test3= "bear";
+
+// var test4 = "mom";
+
+// var array = [test1, test2, test3, test4];
+
+// for (i=0; i < array.length; i++) {
+
+// }
+
+//     if (i.split("").reverse().join("") === i.split("").reverse().join("")) { 
+// console.log("yes palindrome");
+// } else {
+
+// }
+
+
+var test1 = "mom";
+
+var opp = test1.split("").reverse().join("");
+
+if (test1 === opp) {
+    console.log("I'm a palindrome");
+} else {
+    console.log("I'm not a palindrome");
+}
+
