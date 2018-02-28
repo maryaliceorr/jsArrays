26f21c2ff51fd5edaf7cafc6694694d934fae89d
Mary Alice Orr <m.a.orr17@gmail.com>
2018-02-27 21:19:58 -0600

    added last challenge

--- arrays.js
+++ arrays.js
@@ -21,13 +21,60 @@ console.log("hello");
 // }
 
 
-var test1 = "mom";
+// var test1 = "mom";
 
-var opp = test1.split("").reverse().join("");
+// var opp = test1.split("").reverse().join("");
 
-if (test1 === opp) {
-    console.log("I'm a palindrome");
-} else {
-    console.log("I'm not a palindrome");
-}
+// if (test1 === opp) {
+//     console.log("I'm a palindrome");
+// } else {
+//     console.log("I'm not a palindrome");
+// }
+
+
+
+
+
+
+
+// var animals = ["fish", "dog", "cat"]
+
+// var animalzElement = document.getElementById("animalz");
+
+
+
+
+// for (var i=0; i<animals.length; i++) {
+
+// console.log("animal", animal[i])
+// animalizeElement.innerHTML += "<h5>" + animals[i] + "</h5>";
+// }
+
+
+var book1 = "Catch 22";
+
+var book2 = "Slaughterhouse 5";
+
+var book3 = "Farenheit 451";
+
+var newArr = [];
+
+var bookArray = [book1.split(" "), book2.split(" "), book3.split(" ")];
+
+for (var j=0; j<bookArray.length; j++) {
+console.log("book number", bookArray[j].push());
+} 
+
+
+
+// var book = "catch 22";
+// var bookSplit = book.split("");
+// var realNumb = [];
+// for (var i=0; i<bookSplit.length; i++) {
+//     // console.log("bookSplit", bookSplit[i]);
+//     if ((bookSplit[i]*1)) {
+//     realNumb.push(bookSplit[i]);
+//     }
+// }
+//         console.log("answer", realNumb.join(""));
 
